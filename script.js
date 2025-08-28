8fb68d5b53df1f1141236a430f3461f41dab3081
Palok kumar mandol <palokk02@gmail.com>
2025-08-29 01:34:17 +0600

    added heart click functionality

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('file added');
+
+
+const hearts = document.getElementsByClassName('fa-heart');
+for(const heart of hearts){
+    heart.addEventListener('click',function(){
+        const hearCount = parseInt(document.getElementById('hert-cnt').innerText);
+        const currentHeartCount = hearCount + 1;
+        document.getElementById('hert-cnt').innerText = currentHeartCount;
+    })
+}
+
+
+
+
